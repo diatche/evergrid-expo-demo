0c01ca6aa23b8ba807a97c9650e15b4fc257e9b7
Pavel Diatchenko <diatche@gmail.com>
2020-10-15 17:09:12 +1300

    Fixed chart.

--- src/pages/Demos/Chart.tsx
+++ src/pages/Demos/Chart.tsx
@@ -10,7 +10,6 @@ import {
     Button,
 } from 'react-native-paper';
 import {
-    GridLayoutSource,
     LayoutSource,
     RecyclerGridView,
     FlatLayoutSource,
@@ -18,58 +17,122 @@ import {
     IPoint,
 } from 'recyclergridview';
 
-const kStep: IPoint = { x: 10, y: 10 }
+const kInitialScale = 50;
+const kStep: IPoint = { x: 1, y: 1 }
+const kPointRadius = 0.15;
+const kPointDensity = 1;
+const kXAxisHeight = 60;
+const kYAxisWidth = 60;
 
 export default function Chart() {
     const gridViewRef = React.useRef<RecyclerGridView>(null);
-    const scale$ = React.useRef(new Animated.ValueXY({ x: 30, y: -30})).current;
-
-    const grid = React.useRef(new GridLayoutSource({
-        reuseID: 'grid',
-        itemSize: kStep,
-        shouldRenderItem: () => false,
-    })).current;
-
-    const points = React.useRef(new CustomLayoutSource({
-        itemSize: { x: 1, y: 1}, 
-        reuseID: 'point',
-        getItemLayout: i => {
-            i *= 0.1;
-            return {
-                offset: { x: i * 10, y: Math.sin(i) * 5 },
-            };
-        },
-        getVisibleIndexSet: (pointRange) => {
-            let indexSet = new Set<number>();
-            for (
-                let i = Math.floor(pointRange[0].x);
-                i < Math.ceil(pointRange[1].x);
-                i++
-            ) {
-                indexSet.add(i);
-            }
-            return indexSet;
-        },
-        shouldRenderItem: () => false,
-    })).current;
+    const scale$ = React.useRef(new Animated.ValueXY({ x: kInitialScale, y: -kInitialScale})).current;
 
     const [layoutSources] = React.useState<LayoutSource[]>(() => {
         return [
-            grid,
-            points,
             new FlatLayoutSource({
-                reuseID: 'bottom',
-                itemSize: { x: kStep.x, y: 60}, 
+                reuseID: 'verticalGrid',
+                itemSize: kStep,
+                getItemViewLayout: (i, view, layout) => ({
+                    size: {
+                        x: Animated.multiply(layout.getScale$(view).x, layout.itemSize.x),
+                        y: view.containerSize$.y,
+                    }
+                }),
                 horizontal: true,
-                stickyEdge: 'bottom',
-                origin: { x: 0, y: -60 },
-                shouldRenderItem: () => true,
+                stickyEdge: 'top',
+                shouldRenderItem: () => false,
             }),
             new FlatLayoutSource({
-                reuseID: 'right',
-                itemSize: { x: 60, y: kStep.y }, 
+                reuseID: 'horizontalGrid',
+                itemSize: kStep,
+                getItemViewLayout: (i, view, layout) => ({
+                    size: {
+                        x: view.containerSize$.x,
+                        y: Animated.multiply(layout.getScale$(view).y, layout.itemSize.y),
+                    }
+                }),
+                stickyEdge: 'left',
+                shouldRenderItem: () => false,
+            }),
+            new CustomLayoutSource({
+                reuseID: 'point',
+                itemSize: { x: kPointRadius * 2, y: kPointRadius * 2 },
+                itemOrigin: { x: 0.5, y: 0.5 },
+                getItemLayout: i => {
+                    // i *= 0.1;
+                    return {
+                        offset: { x: i / kPointDensity, y: Math.sin(i * Math.PI * 0.1) * 2 },
+                    };
+                },
+                getVisibleIndexSet: (pointRange) => {
+                    let indexSet = new Set<number>();
+                    for (
+                        let i = Math.floor(pointRange[0].x * kPointDensity);
+                        i < Math.ceil(pointRange[1].x * kPointDensity);
+                        i++
+                    ) {
+                        indexSet.add(i);
+                    }
+                    return indexSet;
+                },
+                shouldRenderItem: () => false,
+            }),
+            new CustomLayoutSource({
+                reuseID: 'bottomAxis',
+                getItemViewLayout: (i, view) => ({
+                    offset: {
+                        x: 0,
+                        y: Animated.subtract(view.containerSize$.y, kXAxisHeight),
+                    },
+                    size: {
+                        x: view.containerSize$.x,
+                        y: kXAxisHeight,
+                    },
+                }),
+                getVisibleIndexSet: () => new Set([0]),
+                shouldRenderItem: () => false,
+            }),
+            new FlatLayoutSource({
+                reuseID: 'bottomAxisMajor',
+                itemSize: kStep,
+                getItemViewLayout: () => ({
+                    size: {
+                        x: 60,
+                        y: kXAxisHeight,
+                    }
+                }),
+                horizontal: true,
+                stickyEdge: 'bottom',
+                itemOrigin: { x: 0.5, y: 1 },
+                shouldRenderItem: () => true,
+            }),
+            new CustomLayoutSource({
+                reuseID: 'rightAxis',
+                getItemViewLayout: (i, view) => ({
+                    offset: {
+                        x: Animated.subtract(view.containerSize$.x, kYAxisWidth),
+                        y: 0,
+                    },
+                    size: {
+                        x: kYAxisWidth,
+                        y: view.containerSize$.y,
+                    },
+                }),
+                getVisibleIndexSet: () => new Set([0]),
+                shouldRenderItem: () => false,
+            }),
+            new FlatLayoutSource({
+                reuseID: 'rightAxisMajor',
+                itemSize: kStep,
+                getItemViewLayout: () => ({
+                    size: {
+                        x: kYAxisWidth,
+                        y: 40,
+                    }
+                }),
                 stickyEdge: 'right',
-                origin: { x: -60, y: 0 },
+                itemOrigin: { x: 1, y: 0.5 },
                 shouldRenderItem: () => true,
             }),
         ];
@@ -94,41 +157,36 @@ export default function Chart() {
                 scale={scale$}
                 anchor={{ x: 0.5, y: 0.5 }}
                 layoutSources={layoutSources}
-                renderItem={(
-                    { index, animated, reuseID },
-                    layoutSource
-                ) => {
+                renderItem={({ index, animated, reuseID }) => {
                     switch (reuseID) {
                         case 'point':
+                            return <Animated.View style={[styles.point, {
+                                borderRadius: Animated.divide(animated.viewLayout.size.x, 2),
+                            }]} />;
+                        case 'bottomAxis':
+                            return <View style={styles.bottomAxis} />
+                        case 'bottomAxisMajor':
                             return (
-                                <View style={{
-                                    alignContent: 'center',
-                                    justifyContent: 'center',
-                                }}>
-                                    <View style={styles.point} />
+                                <View style={styles.bottomAxisMajorContainer}>
+                                    <View style={styles.bottomAxisMajorTick} />
+                                    <Text style={styles.bottomAxisMajorLabel}>{index}</Text>
                                 </View>
                             );
-                        case 'grid':
-                            return <View style={styles.grid} />
-                        case 'right':
+                        case 'rightAxis':
+                            return <View style={styles.rightAxis} />
+                        case 'rightAxisMajor':
                             return (
-                                <View style={{
-                                    flex: 1,
-                                    flexDirection: 'row-reverse'
-                                }}>
-                                    <View style={styles.right}>
-                                        <View style={{ flex: 1}} />
-                                        <View style={{
-                                            height: 20,
-                                            marginLeft: 10,
-                                            marginVertical: -10,
-                                            overflow: 'visible',
-                                        }}>
-                                            <Text style={{ color: 'gray' }}>{index}</Text>
-                                        </View>
+                                <View style={styles.rightAxisMajorContainer}>
+                                    <View style={styles.rightAxisMajorTick} />
+                                    <View style={styles.rightAxisMajorLabelContainer}>
+                                        <Text style={styles.rightAxisMajorLabel}>{index}</Text>
                                     </View>
                                 </View>
                             );
+                        case 'horizontalGrid':
+                            return <View style={styles.horizontalGrid} />
+                        case 'verticalGrid':
+                            return <View style={styles.verticalGrid} />
                         default: 
                             return null;
                     }
@@ -148,24 +206,83 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: 'white',
     },
-    grid: {
+    horizontalGrid: {
         flex: 1,
-        borderTopWidth: 0.5,
-        borderLeftWidth: 0.5,
-        borderColor: 'gray',
-        borderStyle: 'dotted'
+        borderTopWidth: 1,
+        borderColor: 'rgba(200, 210, 230, 0.5)',
+        borderStyle: 'dotted',
+        backgroundColor: 'transparent',
     },
-    right: {
-        width: 60,
-        borderLeftWidth: 0.5,
+    verticalGrid: {
+        flex: 1,
+        borderLeftWidth: 1,
+        borderColor: 'rgba(200, 210, 230, 0.5)',
+        borderStyle: 'dotted',
+        backgroundColor: 'transparent',
+    },
+    rightAxis: {
+        flex: 1,
+        borderLeftWidth: 1,
         borderColor: 'gray',
         backgroundColor: 'white',
     },
+    rightAxisMajorContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignContent: 'center',
+        // borderColor: 'gray',
+        // borderWidth: 1,
+    },
+    rightAxisMajorTick: {
+        width: 4,
+        height: 1,
+        alignSelf: 'center',
+        backgroundColor: 'gray',
+    },
+    rightAxisMajorLabelContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    rightAxisMajorLabel: {
+        marginHorizontal: 8,
+        color: 'gray',
+    },
+    bottomAxis: {
+        flex: 1,
+        borderTopWidth: 1,
+        borderColor: 'gray',
+        backgroundColor: 'white',
+    },
+    bottomAxisMajorContainer: {
+        flex: 1,
+        alignContent: 'center',
+        // borderColor: 'gray',
+        // borderWidth: 1,
+    },
+    bottomAxisMajorTick: {
+        width: 1,
+        height: 4,
+        alignSelf: 'center',
+        backgroundColor: 'gray',
+    },
+    bottomAxisMajorLabel: {
+        margin: 5,
+        textAlign: 'center',
+        color: 'gray',
+    },
+    // rightAxis: {
+    //     width: 60,
+    //     borderLeftWidth: 0.5,
+    //     borderColor: 'gray',
+    //     backgroundColor: 'white',
+    // },
     point: {
-        width: 20,
-        height: 20,
-        borderRadius: 10,
-        backgroundColor: 'rgba(100, 130, 160, 0.5)',
+        flex: 1,
+        // width: kPointRadius * 2,
+        // height: kPointRadius * 2,
+        // borderRadius: kPointRadius,
+        backgroundColor: 'rgb(100, 150, 200)',
     },
     toolbar: {
         height: 50,
@@ -173,5 +290,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-around',
+        borderColor: 'gray',
+        borderTopWidth: 1,
     }
 });
